1f5f7bade2a2fc8ca9d18ec79141fbfa404c0612
Scott Martínez <scottmtinez@gmail.com>
2025-02-14 21:17:03 -0600

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
 import Account from './Account';
+import MextPage from './NexPage';
 
 function Home(){
     return (
         <div className='home-container'>
             <Account />
+            <NextPage />
 
             
         </div>
