91282afaf0540635b5946fa08b301c7c8c0c4db5
Scott Martínez <scottmtinez@gmail.com>
2025-02-25 20:36:16 -0600

    Update UsersAccountInfo.js

--- src/components/UsersAccountInfo.js
+++ src/components/UsersAccountInfo.js
@@ -1,1 +1,16 @@
-/* --- --- */
+import React from 'react';
+import './UsersAccountInfo.css';
+import Nav from './Nav';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function UsersAccountInfo() {
+    return (
+        <div className='Account-container'>
+            <Nav />
+            Users Info
+            <footer className='Home-footer'>Package Tracking Web Application | Portfolio Project | 2025</footer>
+        </div>
+    );
+}
+
+export default UsersAccountInfo;
